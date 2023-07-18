f0da25c961026c8328ea781f1edb33477e10475c
YevD <46435006+YevD@users.noreply.github.com>
2023-07-18 23:30:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsMember = document.getElementById("skillsMember");
+    skillsMember.innerHTML = skills;
+}
